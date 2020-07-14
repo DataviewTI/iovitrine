33a388106d5f3f288bb44f7d4ed76d8e1cd29643
Luis Dataview <luis@dataview.com.br>
2020-07-14 11:50:23 -0300

    route update

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,4 +1,29 @@
 <?php
+
+Route::group(['prefix' => 'vitrine'], function () {
+  Route::get('list-frontend', 'VitrineController@list');
+  Route::post('create', 'VitrineController@createFrontEnd');
+});	
+
+
+Route::group(['prefix' => 'admin', 'middleware' => ['web','admin'], 'as' => 'admin.'],function(){
+    Route::group(['prefix' => 'vitrine'], function () {
+    Route::get('/','VitrineController@index');
+    Route::get('list', 'VitrineController@list');
+    Route::get('list-frontend', 'VitrineController@listFrontEnd');
+    Route::get('view/{id}', 'VitrineController@view');
+    Route::post('create', 'VitrineController@create');
+    Route::post('update/{id}', 'VitrineController@update');
+    Route::get('delete/{id}', 'VitrineController@delete');			
+
+    Route::post('formacao/update/{id}', 'VitrineController@formacaoUpdate');
+    Route::get('formacao/view/{id}', 'VitrineController@formacaoView');
+    Route::post('formacao/create', 'VitrineController@formacaoCreate');
+    Route::get('formacao/list/{id?}', 'VitrineController@formacaoList');
+    Route::get('formacao/delete/{id}', 'VitrineController@formacaoDelete');
+  });
+});
+
 Route::group(['prefix' => 'admin', 'middleware' => ['web','admin'], 'as' => 'admin.'],function(){
     Route::group(['prefix' => 'vitrine'], function () {
     Route::get('/','VitrineController@index');
